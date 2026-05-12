c03dd3fc80b465b5007955c2c0728140d92ae96f
Mohamed Noaman <mohamed.halimo213@gmail.com>
2026-05-13 01:40:53 +0300

    add-user-repo

---
+++ CTOS.Web/Repositroies/UserRepo.cs
@@ -0,0 +1,43 @@
+﻿using CTOS.Web.Database.AppDbContext;
+using CTOS.Web.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CTOS.Web.Repositroies
+{
+    public class UserRepo(CtosDbContext context)
+    {
+        public async Task<User?> GetByIdAsync(int id)
+        {
+
+            return await context.Set<User>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+
+            return await context.Set<User>().ToListAsync();
+        }
+
+        public async Task AddAsync(User entity)
+        {
+            await context.AddAsync(entity);
+        }
+
+        public void UpdateAsync(User entity)
+        {
+            context.Update(entity);
+        }
+
+        public void DeleteAsync(User entity)
+        {
+            context.Remove(entity);
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await context.SaveChangesAsync();
+
+
+        }
+}
+}
